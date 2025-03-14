5267bacecc4fee3f461109c841309788a6a292ea
mr-chris-reed <chrree@bhasd.org>
2025-03-14 08:19:34 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    // This static method finds and returns
+    // the smallest integer in the supplied
+    // array 'arr'.
+    public static int min(int [] arr)
+    {
+
+    }
+
+    public static int max(int [] arr)
+    {
+
+    }
+
+    public static double mean(int [] arr)
+    {
+
+    }
+
+    public static double median(int [] arr)
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        
+    }
+
+}
